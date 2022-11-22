0020cf01e3911f8d22c2d419d7369451a991b852
jinyangrao <jirao4753@qq.com>
2022-11-22 22:50:03 +0800

    change sparksql datasource only store datasource url.

--- src/metabase/api/database.clj
+++ src/metabase/api/database.clj
@@ -628,7 +628,7 @@
                                       (sync.schedules/default-randomized-schedule)))
                                   (when (some? auto_run_queries)
                                     {:auto_run_queries auto_run_queries}))))
-        (events/publish-event! :database-create <>))
+        (events/publish-event! :database-create <>)
         (snowplow/track-event! ::snowplow/database-connection-successful
                                api/*current-user-id*
                                {:database engine, :database-id (u/the-id <>), :source :admin}))
